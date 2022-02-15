f7f4d6689886047e1240bc2c7ba88d097714f675
Marina Tsymlianskaia <marinatsy16@gmail.com>
2022-02-15 10:44:06 -0700

    first commit

---
+++ SugarCodingExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SugarCodingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //SugarCoding Version:
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater or equal to nine";
+            Console.WriteLine(response);
+
+
+            // Old Version:
+            //int answer = 4;
+            //string response;
+
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + “greater than or equal to nine”;
+            //}
+
+        }
+    }
+}
